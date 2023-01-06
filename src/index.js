0b8bc281a1b4bc7d7362d8f433eb67fb830c2522
Tan Gia Luong <tangia123@outlook.com.vn>
2023-01-06 22:55:51 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const path = require("path");
-const morgan = require("morgan");
-const { engine } = require("express-handlebars");
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
 
-const router = require("./routes");
+const router = require('./routes');
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use(morgan("combined"));
 // template engine
-app.engine("hbs", engine({ extname: ".hbs" }));
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.engine('hbs', engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Route init
 router(app);
